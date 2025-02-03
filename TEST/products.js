e6fc8210d07dd5ca088bec292613dbe876cc68ac
buddymajki <buddymajki@gmail.com>
2025-02-03 21:34:23 +0100

    Update products.js

--- TEST/products.js
+++ TEST/products.js
@@ -20,22 +20,13 @@ document.addEventListener("DOMContentLoaded", function () {
                     productCard.innerHTML = `
                         <div class="w3-card product-card">
                             <div class="hover-container">
-                                <a href="${product.link}">
-                                    <img class="w3-margin-top product-image" src="${product.image}" alt="${product.name}">
-                                <div class="hover-description">${product.hover || ""}</div> </a>
+                                <img class="w3-margin-top" src="${product.image}" alt="${product.name}">
+                                <div class="hover-description">${product.author || product.brand || ""}</div>
                             </div>
                             <div class="w3-container w3-center">
-                                <p>
-                                    <a href="${product.link}" class="product-link">${product.name} <br>
-                                        <span style="color:lightgrey">${product.author || product.brand || ""}</span>
-                                    </a>
-                                </p>
+                                <p>${product.name} <br> <span style="color:lightgrey">${product.author || product.brand || ""}</span></p>
                                 <p><b>${product.price} CHF</b></p>
-                                <button class="w3-button w3-light-blue w3-margin add-to-basket"
-                                    data-product="${product.name}"
-                                    data-price="${product.price}">
-                                    Add to Basket
-                                </button>
+                                <button class="w3-button w3-light-blue w3-margin" onclick="addToBasket('${product.name}', ${product.price})">Add to Basket</button>
                             </div>
                         </div>
                     `;
